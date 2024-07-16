ede990b42dd0b8f1fb1fce769f6e661ded16a848
Linda Hrdinová <hrlin@seznam.cz>
2024-07-16 10:17:16 +0200

    create a calculator

---
+++ java-calcurator/src/test/java/com/github/LindaHrdinova/javaCalcurator/JavaCalcuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LindaHrdinova.javaCalcurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCalcuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
