731209cb5539b6d9ee72021eeaea970d1dd018ca
Linda Hrdinová <hrlin@seznam.cz>
2024-07-26 10:08:06 +0200

    calculator can +, -, *, / and ^

--- src/main/java/com/github/LindaHrdinova/java_calculator/countdown/CalculatorController.java
+++ src/main/java/com/github/LindaHrdinova/java_calculator/countdown/CalculatorController.java
@@ -1,22 +1,31 @@
 package com.github.LindaHrdinova.java_calculator.countdown;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Odpočet do Vánoc.
- */
 @Controller
+@RequestMapping("/")
 public class CalculatorController {
-
-    public CalculatorController(CalculatorService service) {
-    }
-
 
     @GetMapping
     public ModelAndView calculator() {
-        return new ModelAndView("index");
+        ModelAndView modelAndView = new ModelAndView("index"); // cesta k /index.ftlh
+        modelAndView.addObject("calculator", new CalculatorService());
+        return modelAndView;
+    }
 
+    @PostMapping("/result")
+    public Object form(@Valid @ModelAttribute("form") CalculatorService calculator) {
+
+        return new ModelAndView("result")
+                .addObject("NumberResult", calculator.calculatorOperation(calculator.getNumberOne(), calculator.getNumberTwo(), calculator.getCalcOperator()))
+                .addObject("NumberOne", calculator.getNumberOne())
+                .addObject("NumberTwo", calculator.getNumberTwo())
+                .addObject("calcOperator", calculator.getCalcOperator());
     }
 }
--- src/main/java/com/github/LindaHrdinova/java_calculator/countdown/CalculatorService.java
+++ src/main/java/com/github/LindaHrdinova/java_calculator/countdown/CalculatorService.java
@@ -4,31 +4,66 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    private int numberOne;
-    private int numberTwo;
-    private int numberResume;
+    private double numberOne;
+    private double numberTwo;
+    private double numberResult;
+    private char calcOperator;
 
-    public int getNumberOne() {
+    public double getNumberOne() {
         return numberOne;
     }
 
-    public void setNumberOne(int numberOne) {
+    public void setNumberOne(double numberOne) {
         this.numberOne = numberOne;
     }
 
-    public int getNumberTwo() {
+    public double getNumberTwo() {
         return numberTwo;
     }
 
-    public void setNumberTwo(int numberTwo) {
+    public void setNumberTwo(double numberTwo) {
         this.numberTwo = numberTwo;
     }
 
-    public int getNumberResume() {
-        return numberResume;
+    public double getNumberResult() {
+        return numberResult;
     }
 
-    public void setNumberResume(int numberResume) {
-        this.numberResume = numberResume;
+    public void setNumberResult(double numberResult) {
+        this.numberResult = numberResult;
+    }
+
+    public char getCalcOperator() {
+        return calcOperator;
+    }
+
+    public void setCalcOperator(char calcOperator) {
+        this.calcOperator = calcOperator;
+    }
+
+    public double calculatorOperation(double a, double b, char operation) {
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*': //&times
+                result = a * b;
+                break;
+            case '/':
+                result = ((a * 100) / b) / 100;
+                break;
+            case '^':
+                result = Math.pow(a, b); // Math.pow je fce pro exponent
+            break;
+            default:
+                System.err.println("Neznámý operátor");
+                break;
+        }
+        return result;
     }
 }
